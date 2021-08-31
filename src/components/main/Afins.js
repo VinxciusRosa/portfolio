708cc8544564ab60688092fe2ca81767ad14f93a
Vinicius <v.rosa@aluno.ifsp.edu.br>
2021-08-31 01:36:16 -0300

    estrutura da página 100% completa

--- src/components/main/Afins.js
+++ src/components/main/Afins.js
@@ -23,9 +23,9 @@ export default class Afins extends React.Component {
     
         return (
             <div id='afins'>
-               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#690394" fill-opacity="0.6" d="M0,96L60,80C120,64,240,32,360,48C480,64,600,128,720,133.3C840,139,960,85,1080,58.7C1200,32,1320,32,1380,32L1440,32L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
-                <h1 className='title-afins'>Afins...</h1>
-
+               <h1 className='title-afins'> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 150"><path fill="#690394" fill-opacity="0.6" d="M0,96L60,80C120,64,240,32,360,48C480,64,600,128,720,133.3C840,139,960,85,1080,58.7C1200,32,1320,32,1380,32L1440,32L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
+               Afins...</h1>
+                
                 <div className='publicacoes'>
                     <div className='title-publi'>
                         <h1>Publicações</h1>
