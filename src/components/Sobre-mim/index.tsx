b02d76bfe8b73c470223a51f13c6e9104bd83631
VinxciusRosa <v.rosa@aluno.ifsp.edu.br>
2024-03-30 13:58:43 -0300

    atualizacao cv

--- src/components/Sobre-mim/index.tsx
+++ src/components/Sobre-mim/index.tsx
@@ -15,7 +15,7 @@ export function Sobre(){
 				<p>My name is Vinicius Carvalho Rosa, I am 23 years old. I graduated in Bacheleor of Science (BSc) in Systems Analysis and Development from the Federal Institute of São Paulo, with emphasis on a scientific initiation and extension scholarship, contributing to the publication of 8 articles in scientific journals and periodical annals, addressing interfaces on the topics: black holes, education and computing.
 					<br/><br/>
 					I have 3 years of experience in developing data solutions on the Microsoft Azure platform, I highlight my specialization in creating ETL pipelines and process optimization. Using tools such as Apache Spark, Python, SQL, Databricks, Data Factory, Data Lake and Synapse Analytics.				</p>
-				<a href="" >
+				<a href={CV} >
 					<button><AiOutlineCloudDownload/>Curriculum</button>
 				</a>
 			</div>
